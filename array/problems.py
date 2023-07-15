3090d9653e6d00d76b3070f98dcf651a1c6e582c
mendacium <sakshamlinkedin@gmail.com>
2023-07-16 00:13:14 +0530

    return max and min in an array

--- array/problems.py
+++ array/problems.py
@@ -9,4 +9,23 @@ def reverseArr(arr):
 
 arr1 = [1,2,3,4,5,6,7,8,9]
 
-print(reverseArr(arr1))
+# print(reverseArr(arr1))
+
+#find max and min in array
+
+def maxMin(arr):
+    min = arr[0]
+    max = arr[0]
+    
+    for i in range(1,len(arr)):
+        if (arr[i] > max):
+            max = arr[i]
+        elif (arr[i] < min):
+            max = arr[i]
+
+    return f"min:{min} max:{max}"  
+
+
+
+arr2 = [2,5,3,7,13,44,64,5,99,47,36]
+print(maxMin(arr2))
