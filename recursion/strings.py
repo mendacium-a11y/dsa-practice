d8f64de7eeb7db4ffc7707eddc9ae15df59af47c
mendacium <sakshamlinkedin@gmail.com>
2023-08-01 23:59:27 +0530

    palindrome check using recursion

--- recursion/strings.py
+++ recursion/strings.py
@@ -9,7 +9,23 @@ def reverse (arr,i,j) :
     j -= 1
     return reverse(arr,i,j)
 
-str1 = "abdce"
+# str1 = "abdce"
+# 
+# a = reverse(list(str1),0,4)
+# print("".join(a))
 
-a = reverse(list(str1),0,4)
-print("".join(a))
+# palindrome checker
+def palindrome(arr,i,j):
+    if i > j:
+        return True
+    elif arr[i] != arr[j]:
+        return False
+    else:
+
+        i += 1
+        j -= 1
+        return palindrome(arr, i, j)
+
+str1 = "aaaaabaaa"
+a = palindrome(str1, 0, len(str1)-1)
+print(a)
