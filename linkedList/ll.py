ea01d64dce7e98c18dada5557d6e6793ef15e2a1
mendacium <sakshamlinkedin@gmail.com>
2023-08-24 22:13:30 +0530

    implemented simple linked list

--- linkedList/ll.py
+++ linkedList/ll.py
@@ -15,9 +15,39 @@ class LinkedList:
         
     def insertAtTail(self, data):
         newNode = Node(data)
-        if (!self.head):
+        if (not self.head):
             self.head = newNode
+            self.tail = newNode
+            return
+
         current = self.head
         while(current.next):
             current = current.next
+        current.next = newNode
+        self.tail = newNode
+
+    def insertAtPosition(self,data,position):
+        if(position <= 0):
+            self.insertAtHead(data)
+            return
+        newNode = Node(data)
+        current = self.head
+        temp = 1
+        while(current.next and temp <= position):
+            if(temp+1 == position):
+                newNode.next = current.next
+                current.next = newNode
+                return
+                
+            current = current.next
+            temp +=1
+    
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+
         
